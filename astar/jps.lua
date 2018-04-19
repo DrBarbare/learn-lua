9e5ce6f075769e24a331d72ad602b327d9809e5a
Jean-Raymond Raynal <jeanraymond.raynal@gmail.com>
2018-04-18 22:49:06 -0400

    More test and fixes for JPS

--- astar/jps.lua
+++ astar/jps.lua
@@ -22,14 +22,14 @@ local permutation = {
 	['W'] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 },
 	['S'] = { 3, 6, 9, 2, 5, 8, 1, 4, 7 },
 	-- Diagonal
-	['NE'] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-	['SE'] = { 7, 4, 1, 8, 5, 2, 9, 6, 3 },
-	['SW'] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 },
-	['NW'] = { 3, 6, 9, 2, 5, 8, 1, 4, 7 }
+	['SE'] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, --NE
+	['NE'] = { 7, 4, 1, 8, 5, 2, 9, 6, 3 }, --SE
+	['NW'] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, --SW
+	['SW'] = { 3, 6, 9, 2, 5, 8, 1, 4, 7 }  --NW
 }
 
 -- List of orientation OUT of the box
-local orientationDelta = {
+local neighborDelta = {
 	{x=-1,y=-1 }, -- NW
 	{x= 0,y=-1 }, -- N
 	{x= 1,y=-1 }, -- NE
@@ -60,23 +60,24 @@ local function isGoal(cdt, tgt)
 end
 
 local function coordinates(pos, delta)
-	local nextPos = {}
-	nextPos.x = pos.x + delta.x
-	nextPos.y = pos.y + delta.y
+	local nextPos = {
+		x = pos.x + delta.x,
+		y = pos.y + delta.y
+	}
 	return nextPos
 end
 
-local function forcedNeighbor(pathable, pos, ori)
+local function forcedNeighbor(pathable, pos, ori, tgtPos)
 	local orilen = string.len(ori) -- Straight has one letter, diagonal has 2
 
 	local permu  = permutation[ori]
 	local coords = function(index)
 		local p = permu[index]
 		--print("For index: " .. index .. " got " .. p)
-		local o = orientationDelta[p]
-		--print("For index: " .. index .. " got " .. p .. " -> {" .. o.x .. "x" .. o.y .. "}")
+		local o = neighborDelta[p]
+		--print("For index: " .. index .. " got " .. p .. " -> Delta{" .. o.x .. "x" .. o.y .. "}")
 		local c = coordinates(pos, o)
-		--print("For index: " .. index .. " got " .. p .. " -> {" .. o.x .. "x" .. o.y .. "} -> {" .. c.x .. "x" .. c.y .. "}")
+	--	print("For index: " .. index .. " got " .. p .. " -> Delta{" .. o.x .. "x" .. o.y .. "} -> Coods{" .. c.x .. "x" .. c.y .. "}")
 		return c
 	end
 	local checkNeighbor = function(index)
@@ -86,14 +87,14 @@ local function forcedNeighbor(pathable, pos, ori)
 
 
 	-- Diagonal case
-	-- 1 2 3
+	-- \ 2 3
 	-- 4 X 6
-	-- / 8 9
+	-- 7 8 9
 	if orilen == 2 then
-		if not checkNeighbor(4) then return { pos = coords(1) } end
-		if not checkNeighbor(8) then return { pos = coords(9) } end
-		return jump(pathable, pos, deltas[ str:sub(1, 1, ori) ], tgtPos)
-		       or jump(pathable, pos, deltas[ str:sub(2, 2, ori) ], tgtPos)
+		if not checkNeighbor(2) then return { pos = coords(3) } end
+		if not checkNeighbor(4) then return { pos = coords(7) } end
+		return jump(pathable, pos, deltas[ string.sub(ori, 1, 1) ], tgtPos)
+		       or jump(pathable, pos, deltas[ string.sub(ori, 2, 2) ], tgtPos)
 
 	-- Straight case
 	-- 1 2 3
@@ -107,8 +108,8 @@ local function forcedNeighbor(pathable, pos, ori)
 end
 
 jump = function(pathable, pos, delta, tgtPos)
-	nextPos = coordinates(pos, delta)
-	--print("Next pos: " .. nextPos.x .. "x" .. nextPos.y)
+	local nextPos = coordinates(pos, delta)
+	-- print("Jump from: {" .. nextPos.x .. "x" .. nextPos.y .. "} -> {" .. tgtPos.x .. "x" .. tgtPos.y .. "}")
 
 	if not pathable(nextPos.x, nextPos.y) then return nil end
 	if isGoal(nextPos, tgtPos) then return { pos = nextPos } end
@@ -116,7 +117,8 @@ jump = function(pathable, pos, delta, tgtPos)
 	-- Retrive orientation
 	ori = orientation(delta)
 
-	return forcedNeighbor(pathable, nextPos, ori) or jump(pathable, nextPos, delta, tgtPos)	
+	return forcedNeighbor(pathable, nextPos, ori, tgtPos)
+	       or jump(pathable, nextPos, delta, tgtPos)	
 end
 
 return jump
--- astar/tests/jps/main.lua
+++ astar/tests/jps/main.lua
@@ -1,6 +1,6 @@
-require '../jps'
+require 'jps'
 
-function testHorizontal()
+function testSimple()
 	local map={
 		{ 1, 1, 1, 1, 1, 1 },
 		{ 1, 1, 1, 1, 1, 1 },
@@ -10,37 +10,84 @@ function testHorizontal()
 	}
 
 	local pathable = function(x, y)
-		local answer = false
-		if not map then
---			print "No Map"
-		elseif not map[y] then
---			print("No Row #" .. y .. "/" .. #map)
-		elseif not map[y][x] then
---			print("No col #" .. x .. "/" .. #map[y])
-		elseif map[y][x] == 1 then
-			answer = true
-		else
---			print("Not a pathable value: " .. map[y][x])
-		end
-		return answer
+		return map and y <= #map and map[y]
+		       and x <= #map[y] and map[y][x] and map[y][x] == 1
 	end
 
 	local check = function(src, tgt, delta)
-		print("Testing: " .. src.x .. "x" .. src.y .. " -> " .. tgt.x .. "x" .. tgt.y .. " [" .. delta.x .. "x" .. delta.y .. "]")
+		-- print("Testing: " .. src.x .. "x" .. src.y .. " -> " .. tgt.x .. "x" .. tgt.y .. " [" .. delta.x .. "x" .. delta.y .. "]")
 		local result = jump(pathable, src, delta, tgt)
-		--if result then
-		--	print("Result: " .. result.pos.x .. "x" .. result.pos.y .. " expected " .. tgt.x .. "x" .. tgt.y)
-		--else
-		--	print ("No result")
-		--end
 		assert(result and result.pos.x == tgt.x  and result.pos.y == tgt.y)
 	end
-	-- Horizontal
+	print ">>> Horizontal"
 	check({ x = 2, y = 3 }, { x = 5, y = 3 }, { x = 1,  y = 0 }) -- l -> r
 	check({ x = 5, y = 3 }, { x = 2, y = 3 }, { x =-1,  y = 0 }) -- l <- r
-	-- Vertical
+	print ">>> Vertical"
 	check({ x = 3, y = 1 }, { x = 3, y = 5 }, { x = 0,  y = 1 }) -- t -> d
 	check({ x = 3, y = 5 }, { x = 3, y = 1 }, { x = 0,  y =-1 }) -- t <- d
+
+	print ">>> Diagonal"
+	check({ x = 2, y = 1 }, { x = 5, y = 4 }, { x = 1,  y = 1 }) -- tl -> br
+	check({ x = 5, y = 4 }, { x = 2, y = 1 }, { x =-1,  y =-1 }) -- br -> tl
+	check({ x = 5, y = 1 }, { x = 2, y = 4 }, { x =-1,  y = 1 }) -- tr -> bl
+	check({ x = 2, y = 4 }, { x = 5, y = 1 }, { x = 1,  y =-1 }) -- bl -> tr
+
+end
+
+function testSimpleWithStop()
+	local map={
+		{ 1, 1, 1, 1, 1, },
+		{ 1, 1, 1, 1, 1, },
+		{ 1, 1, 0, 1, 1, },
+		{ 1, 1, 1, 1, 1, },
+		{ 1, 1, 1, 1, 1, }
+	}
+
+	local pathable = function(x, y)
+		return map and y <= #map and map[y]
+		       and x <= #map[y] and map[y][x] and map[y][x] == 1
+	end
+
+	local check = function(src, tgt, delta, expectedPos)
+		print("Testing: " .. src.x .. "x" .. src.y .. " -> " .. tgt.x .. "x" .. tgt.y .. " [" .. delta.x .. "x" .. delta.y .. "]")
+		local result = jump(pathable, src, delta, tgt)
+		if result then
+			print("Result {" .. result.pos.x .. "x" .. result.pos.y .. "}")
+		else
+			print("No result")
+		end
+		assert(result and result.pos.x == expectedPos.x  and result.pos.y == expectedPos.y)
+	end
+	print ">>> Horizontal"
+	--check({ x = 2, y = 3 }, { x = 5, y = 3 }, { x = 1,  y = 0 }, { x = 3, y = 2 }) -- l -> r
+	check({ x = 2, y = 2 }, { x = 5, y = 2 }, { x = 1,  y = 0 }, { x = 4, y = 3 }) -- l -> r
+	check({ x = 2, y = 4 }, { x = 5, y = 4 }, { x = 1,  y = 0 }, { x = 4, y = 3 }) -- l -> r
+	--check({ x = 5, y = 3 }, { x = 2, y = 3 }, { x =-1,  y = 0 }) -- l <- r
+	check({ x = 5, y = 2 }, { x = 2, y = 2 }, { x =-1,  y = 0 }, { x = 2, y = 3 }) -- l <- r
+	check({ x = 5, y = 4 }, { x = 2, y = 4 }, { x =-1,  y = 0 }, { x = 2, y = 3 }) -- l <- r
+	print ">>> Vertical"
+	--check({ x = 3, y = 1 }, { x = 3, y = 5 }, { x = 0,  y = 1 }) -- t -> d
+	check({ x = 2, y = 2 }, { x = 2, y = 4 }, { x = 0,  y = 1 }, { x = 3, y = 4 }) -- l -> r
+	check({ x = 4, y = 2 }, { x = 4, y = 4 }, { x = 0,  y = 1 }, { x = 3, y = 4 }) -- l -> r
+	--check({ x = 3, y = 5 }, { x = 3, y = 1 }, { x = 0,  y =-1 }) -- t <- d
+	check({ x = 2, y = 4 }, { x = 2, y = 2 }, { x = 0,  y =-1 }, { x = 3, y = 2 }) -- l <- r
+	check({ x = 4, y = 4 }, { x = 4, y = 2 }, { x = 0,  y =-1 }, { x = 3, y = 2 }) -- l <- r
+
+	print ">>> Diagonal"
+	check({ x = 1, y = 2 }, { x = 4, y = 5 }, { x = 1,  y = 1 }, { x = 4, y = 3 })
+	check({ x = 4, y = 5 }, { x = 1, y = 2 }, { x =-1,  y =-1 }, { x = 3, y = 2 })
+	check({ x = 2, y = 1 }, { x = 5, y = 4 }, { x = 1,  y = 1 }, { x = 3, y = 4 })
+	check({ x = 5, y = 4 }, { x = 2, y = 1 }, { x =-1,  y =-1 }, { x = 2, y = 3 })
+
+	check({ x = 2, y = 5 }, { x = 5, y = 2 }, { x = 1,  y =-1 }, { x = 3, y = 2 })
+	check({ x = 5, y = 2 }, { x = 2, y = 5 }, { x =-1,  y = 1 }, { x = 2, y = 3 })
+
+	check({ x = 1, y = 4 }, { x = 4, y = 1 }, { x = 1,  y =-1 }, { x = 4, y = 3 })
+	check({ x = 4, y = 1 }, { x = 1, y = 4 }, { x =-1,  y = 1 }, { x = 3, y = 4 })
+	--check({ x = 2, y = 1 }, { x = 5, y = 4 }, { x = 1,  y = 1 }) -- tl -> br
+	--check({ x = 5, y = 4 }, { x = 2, y = 1 }, { x =-1,  y =-1 }) -- br -> tl
+	--check({ x = 5, y = 1 }, { x = 2, y = 4 }, { x =-1,  y = 1 }) -- tr -> bl
+	--check({ x = 2, y = 4 }, { x = 5, y = 1 }, { x = 1,  y =-1 }) -- bl -> tr
 
 end
 
@@ -49,7 +96,10 @@ function love.keypressed(key)
 end
 
 function love.load()
-	testHorizontal()
+	print("=========================================\n >>>>>>>>> Simple test \n ==============================================")
+	testSimple()
+	print("=========================================\n >>>>>>>>> Simple test With stop \n ==============================================")
+	testSimpleWithStop()
 end
 
 
